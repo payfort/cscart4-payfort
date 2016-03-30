d0aece83763034730b7afa6c7bcbd317527b3645
msaleh@payfort.com <msaleh@payfort.com>
2016-03-30 17:39:03 +0200

    merchant page mobile compatibility

--- js/addons/payfort_fort/checkout.js
+++ js/addons/payfort_fort/checkout.js
@@ -4,7 +4,6 @@ function showMerchantPage(merchantPageUrl) {
     }
     $("#review-buttons-container .btn-checkout").hide();
     $("#review-please-wait").show();
-    
     $('<iframe  name="payfort_merchant_page" id="payfort_merchant_page"height="550px" frameborder="0" scrolling="no" onload="pfIframeLoaded(this)" style="display:none"></iframe>').appendTo('#pf_iframe_content');
     $('.pf-iframe-spin').show();
     $('.pf-iframe-close').hide();
@@ -13,7 +12,18 @@ function showMerchantPage(merchantPageUrl) {
     $( "#payfort_payment_form" ).attr("target","payfort_merchant_page");
     $( "#payfort_payment_form" ).children('.cm-no-hide-input').remove();
     $( "#payfort_payment_form" ).submit();
+    //fix for touch devices
+    if (fn_payfort_fort_is_touch_device()) {
+        setTimeout(function() {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        }, 1);
+    }
     $( "#div-pf-iframe" ).show();
+}
+
+function fn_payfort_fort_is_touch_device() {
+  return 'ontouchstart' in window        // works on most browsers 
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
 }
 
 function pfClosePopup() {
