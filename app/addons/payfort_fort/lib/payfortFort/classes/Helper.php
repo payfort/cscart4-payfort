2e90a4f3cd837413cc967de1432676f0a74e123e
Deya Zalloum <dzalloum@payfort.com>
2017-01-03 13:55:58 +0200

    change offline notification api url

--- app/addons/payfort_fort/lib/payfortFort/classes/Helper.php
+++ app/addons/payfort_fort/lib/payfortFort/classes/Helper.php
@@ -146,8 +146,8 @@ class Payfort_Fort_Helper
 
     public function getGatewayHost()
     {
-        if ($this->isSandboxMode()) {
-            return $this->getGatewayProdHost();
+        if ($this->pfConfig->isSandboxMode()) {
+            return $this->getGatewaySandboxHost();
         }
         return $this->getGatewayProdHost();
     }
@@ -156,7 +156,7 @@ class Payfort_Fort_Helper
     {
         $testMode = $this->pfConfig->isSandboxMode();
         if ($type == 'notificationApi') {
-            $gatewayUrl = $testMode ? $this->pfConfig->getGatewaySandboxHost() . 'FortAPI/paymentApi' : $this->pfConfig->getGatewayProdHost() . 'FortAPI/paymentApi';
+            $gatewayUrl = $testMode ?  'https://sbpaymentservices.payfort.com/FortAPI/paymentApi' :  'https://paymentservices.payfort.com/FortAPI/paymentApi';
         }
         else {
             $gatewayUrl = $testMode ? $this->pfConfig->getGatewaySandboxHost() . 'FortAPI/paymentPage' : $this->pfConfig->getGatewayProdHost() . 'FortAPI/paymentPage';
